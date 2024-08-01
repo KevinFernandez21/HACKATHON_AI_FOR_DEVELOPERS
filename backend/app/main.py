63af9219ff0014fdb03390d65e23b7813ea1c338
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-08-01 07:04:57 -0500

    test vision Gemini

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 import google.generativeai as genai 
 from dotenv import load_dotenv
 import os
+import time
+
 load_dotenv()  # Load the .env file
 
-genai.configure(api_key = os.getenv('API_KEY'))
+genai.configure(api_key=os.getenv('API_KEY'))
 
-model = genai.GenerativeModel('gemini-1.5-pro')
-print(model)
+model = genai.GenerativeModel('gemini-1.5-flash')# 'gemini-1.5-pro' 
 
+# Start the timer
+start_time = time.time()
+
+response = model.generate_content("Podrias darme recomendaciones para poder realizar un proyecto de inteligencia artificial?")
+
+# End the timer
+end_time = time.time()
+
+# Calculate the elapsed time
+elapsed_time = end_time - start_time
+
+print(response.text)
+print(f"Tiempo de respuesta: {elapsed_time} segundos")
