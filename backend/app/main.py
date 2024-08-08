95816bdc68ffaa3a4e67ddbb4f086d06e36355f2
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-08-07 19:14:01 -0500

    update Main

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,4 +45,4 @@ def generate_content(request: RequestBody):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
