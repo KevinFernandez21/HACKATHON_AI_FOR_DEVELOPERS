7e1751714414fb307363798ab3d3e660327092e4
Maruu03 <maanvelo@espol.edu.ec>
2024-08-09 16:00:16 -0500

    prompt historial
    se agregó un prompt para que contesté simple complex or nothing

--- backend/app/tests/Mariu.py
+++ backend/app/tests/Mariu.py
@@ -26,8 +26,22 @@ def chatbotGemini(pregunta: GeminiSchema):
 
   model = genai.GenerativeModel( model_name="gemini-1.5-flash" )
 
-  chat = model.start_chat(history=[])
-
-  response = chat.send_message(pregunta.preguntas)
+  chat_session = model.start_chat(
+    history=[
+      {
+        "role": "user",
+        "parts": [
+          "You can only say 3 words Simple, Complex and nothing, under your criteria you can say simple when the user asks for a simple task that does not need planning, you can say Complex when the user needs a schedule or many tasks and finally if neither of the two or both are the case say nothing, you are an App against procrastination and improving time control you cannot talk about anything else",
+        ],
+      },
+      {
+        "role": "model",
+        "parts": [
+          "Okay, I understand. I will respond with only \"Simple\", \"Complex\", or \"Nothing\" based on your request. Let's begin! 😊 \n",
+        ],
+      },
+    ]
+  )
+  response = chat_session.send_message(pregunta.preguntas)
 
   return JSONResponse(status_code=status.HTTP_200_OK, content={"respuesta": response.text})
--- backend/app/tests/gemP.py
+++ backend/app/tests/gemP.py
@@ -14,11 +14,26 @@ generation_config = {
 model = genai.GenerativeModel( model_name="gemini-1.5-flash",
     generation_config=generation_config, )
 
-chat = model.start_chat(history=[])
+chat_session = model.start_chat(
+  history=[
+    {
+      "role": "user",
+      "parts": [
+        "You can only say 3 words Simple, Complex and nothing, under your criteria you can say simple when the user asks for a simple task that does not need planning, you can say Complex when the user needs a schedule or many tasks and finally if neither of the two or both are the case say nothing, you are an App against procrastination and improving time control you cannot talk about anything else",
+      ],
+    },
+    {
+      "role": "model",
+      "parts": [
+        "Okay, I understand. I will respond with only \"Simple\", \"Complex\", or \"Nothing\" based on your request. Let's begin! 😊 \n",
+      ],
+    },
+  ]
+)
 
 while True:
     prompt = input("Ask me anything: ")
     if (prompt == "exit"):
         break
-    response = chat.send_message(prompt)
-    print(response)
+    response = chat_session.send_message(prompt)
+    print(response.text)
