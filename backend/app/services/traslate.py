4b452016d1ecf174db9c615479d141d1790ebf7b
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-08-08 23:47:58 -0500

    update dockerfile

--- backend/app/services/traslate.py
+++ backend/app/services/traslate.py
@@ -1,11 +1,9 @@
 def translate_text(target: str, text: str) -> dict:
     """Translates text into the target language.
-
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
     from google.cloud import translate_v2 as translate
-
     translate_client = translate.Client()
 
     if isinstance(text, bytes):
