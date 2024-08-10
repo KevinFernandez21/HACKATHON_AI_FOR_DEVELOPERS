1c0f744b2e43cc7a9764604482f86dd583699fd2
Darvier88 <127569059+Darvier88@users.noreply.github.com>
2024-08-10 01:07:18 -0500

    Pantallas pasadas a flutter

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,121 +5,258 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); //fl Usando super.key
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: QuizScreen1(),
     );
   }
 }
+class QuizAnswers {
+  static List<String> selectedAnswers = [];
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  static void saveAnswer(String answer) {
+    selectedAnswers.add(answer);
+  }
+}
+class QuizTemplate extends StatefulWidget {
+  final String question;
+  final List<String> options;
+  final Function(String) onNext;
+  final Color backgroundColor;
+  final Color buttonColor;
+  final Function() onNextScreen;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const QuizTemplate({
+    super.key,
+    required this.question,
+    required this.options,
+    required this.onNext,
+    required this.backgroundColor,
+    required this.buttonColor,
+    required this.onNextScreen,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  QuizTemplateState createState() => QuizTemplateState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class QuizTemplateState extends State<QuizTemplate> {
+  String? selectedOption;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: widget.backgroundColor,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          width: 300,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'Tiempo Social',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                widget.question,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20.0),
+              for (var option in widget.options)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedOption = option;  // Actualiza la opción seleccionada
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: selectedOption == option
+                          ? widget.buttonColor // Color del botón sombreado
+                          : Colors.white,
+                      foregroundColor: selectedOption == option
+                          ? Colors.white
+                          : Colors.black,
+                      side: BorderSide(
+                        color: selectedOption == option
+                            ? widget.buttonColor
+                            : Colors.grey,
+                      ),
+                    ),
+                    child: Text(option),
+                  ),
+                ),
+              const SizedBox(height: 20.0),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (selectedOption != null) {
+                      widget.onNext(selectedOption!); // Guarda solo la última opción seleccionada
+                      widget.onNextScreen(); // Navega a la siguiente pantalla
+                    } else {
+                      // Mostrar un mensaje de error si no se ha seleccionado ninguna opción
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Por favor selecciona una opción antes de continuar')),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: widget.buttonColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0,
+                      vertical: 12.0,
+                    ),
+                  ),
+                  child: const Text('Siguiente'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class QuizScreen1 extends StatelessWidget {
+  const QuizScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QuizTemplate(
+      question: 'Dispongo de suficiente tiempo para dedicarlo a mi familia, amigos, pasatiempos y ocio.',
+      options: const ['Casi nunca', 'A veces', 'Casi siempre'],
+      onNext: (String answer) {
+        QuizAnswers.saveAnswer(answer); // Guarda la respuesta seleccionada
+      },
+      backgroundColor: Colors.pink[50]!, // Fondo rosa claro
+      buttonColor: Colors.pink, // Botón color rosa
+      onNextScreen: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const QuizScreen2()),
+        );
+      },
+    );
+  }
+}
+class QuizScreen2 extends StatelessWidget {
+  const QuizScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QuizTemplate(
+      question: 'Dispongo de suficiente tiempo para dedicarlo a mi familia, amigos, pasatiempos y ocio.',
+      options: const ['Casi nunca', 'A veces', 'Casi siempre'],
+      onNext: (String answer) {
+        QuizAnswers.saveAnswer(answer); // Guarda la respuesta seleccionada
+      },
+      backgroundColor: Colors.blue[50]!, // Fondo azul claro
+      buttonColor: Colors.pink, // Botón color rosa
+      onNextScreen: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const QuizScreen3()),
+        );
+      },
+    );
+  }
+}
+class QuizScreen3 extends StatelessWidget {
+  const QuizScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QuizTemplate(
+      question: 'Dispongo de suficiente tiempo para dedicarlo a mi familia, amigos, pasatiempos y ocio.',
+      options: const ['Casi nunca', 'A veces', 'Casi siempre'],
+      onNext: (String answer) {
+        QuizAnswers.saveAnswer(answer); // Guarda la respuesta seleccionada
+      },
+      backgroundColor: Colors.blue[50]!, // Fondo azul claro
+      buttonColor: Colors.pink, // Botón color rosa
+      onNextScreen: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const QuizScreen4()),
+        );
+      },
+    );
+  }
+}
+class QuizScreen4 extends StatelessWidget {
+  const QuizScreen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QuizTemplate(
+      question: 'Dispongo de suficiente tiempo para dedicarlo a mi familia, amigos, pasatiempos y ocio.',
+      options: const ['Casi nunca', 'A veces', 'Casi siempre'],
+      onNext: (String answer) {
+        QuizAnswers.saveAnswer(answer); // Guarda la respuesta seleccionada
+      },
+      backgroundColor: Colors.blue[50]!, // Fondo azul claro
+      buttonColor: Colors.pink, // Botón color rosa
+      onNextScreen: () {
+        // Aquí puedes manejar la acción final, por ejemplo, navegar a una pantalla de resultados
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const ResultScreen()),
+        );
+      },
+    );
+  }
+}
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Resultados'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: QuizAnswers.selectedAnswers.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text('${index + 1}'), // Número de la pregunta
+              ),
+              title: Text('Pregunta ${index + 1}:'),
+              subtitle: Text('Respuesta: ${QuizAnswers.selectedAnswers[index]}'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
