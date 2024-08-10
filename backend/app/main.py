a6606f91b0a8dccef3dfa9d8e4a7c1e95a252f27
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-08-10 10:35:45 -0500

    update dockerfile,main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 import time
 import google.generativeai as genai
-from fastapi.middleware.cors import CORSMiddleware
 
 # Load the .env file
 load_dotenv()
@@ -26,6 +28,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Montar el directorio estático
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Manejar solicitudes para favicon.ico
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    # Retornar el archivo favicon.ico si existe, de lo contrario, retornar un estado 204
+    favicon_path = "static/favicon.ico"
+    if os.path.exists(favicon_path):
+        return FileResponse(favicon_path)
+    return Response(status_code=204)
+
 class RequestBody(BaseModel):
     prompt: str
 
@@ -34,7 +48,8 @@ def generate_content(request: RequestBody):
     try:
         model = genai.GenerativeModel(
             model_name="gemini-1.5-flash",
-            system_instruction="You can only say 3 words Simple Complex and nothing, under your criteria you can say simple when the user asks for a simple task that does not need planning, you can say Complex when the user needs a schedule or many tasks and finally if neither of the two or both are the case say nothing, you are an App against procrastination and improving time control you cannot talk about anything else")
+            system_instruction="You can only say 3 words Simple Complex and nothing, under your criteria you can say simple when the user asks for a simple task that does not need planning, you can say Complex when the user needs a schedule or many tasks and finally if neither of the two or both are the case say nothing, you are an App against procrastination and improving time control you cannot talk about anything else"
+        )
         start_time = time.time()
         response = model.generate_content(request.prompt)
         end_time = time.time()
@@ -45,5 +60,5 @@ def generate_content(request: RequestBody):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT",8000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
